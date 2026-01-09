609c6cf148f5c62eb2abbcfef54e180589e7ed88
Tash <tsukimi.ukiyo@gmail.com>
2026-01-09 23:22:35 +0000

    Added function 'add'/insert function to insert object into db, enums for flagging the entry types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <sqlite3.h>
+#include <string>
 
+enum DBFlag {STUDENT};
+enum COMMAND {ADD, REMOVE, CHANGE};
+
+
+struct Student {
+    std::string name, surname, sex, dob;
+};
 
 sqlite3* openDatabase(const char* filename) {
     sqlite3* db = nullptr;  // declare pointer
@@ -14,14 +22,42 @@ sqlite3* openDatabase(const char* filename) {
     return db; // return the sqlite3* pointer
 }
 
-void create_student(const char* name, const char* surname, const char* sex, const char* dob) {
-    std::string sql_command = std::string("INSERT INTO Student VALUES ('") 
-                        + name + "', '" 
-                        + surname + "', '" 
-                        + sex + "', '" 
-                        + dob + "');";
+void student_func(sqlite3* db, Student student, COMMAND flag) {
+        int rc;
+        sqlite3_stmt* stmt;
+        char* zErrMsg = nullptr;
+        auto callback = [](void*, int, char**, char**) -> int { return 0; };
+        switch (flag){        
+            case ADD:
+                
+                sqlite3_prepare_v2(db, "INSERT INTO Student(name, surname, sex, dob) VALUES (?, ?, ?, ?);", -1, &stmt, nullptr);
+                sqlite3_bind_text(stmt, 1, student.name.c_str(), -1, SQLITE_STATIC);
+                sqlite3_bind_text(stmt, 2, student.surname.c_str(), -1, SQLITE_STATIC);
+                sqlite3_bind_text(stmt, 3, student.sex.c_str(), -1, SQLITE_STATIC);
+                sqlite3_bind_text(stmt, 4, student.dob.c_str(), -1, SQLITE_STATIC);
+                if (sqlite3_step(stmt) != SQLITE_DONE) {
+                    std::cerr << "Failed to execute statement: " << sqlite3_errmsg(db) << std::endl;
+                } else {
+                    std::cout << "Student inserted successfully!" << std::endl;
+                }
 
-    std::cout << sql_command;
+                sqlite3_finalize(stmt);
+
+
+                
+        }
+}
+
+Student create_student(const char* name, const char* surname, const char* sex, const char* dob) {
+    Student student;
+    student.name = name;
+    student.surname = surname;
+    student.sex = sex;
+    student.dob = dob;
+    
+    
+    return student;
+    
 }
 
 int main() {
@@ -30,8 +66,8 @@ int main() {
     if (myDB) {
         std::cout << "Database opened successfully!" << std::endl;
         
-        create_student("Tash", "Not Tash", "M", "07-05-2003");
-
+        Student student = create_student("Tash", "Not Tash", "M", "07-05-2003");
+        student_func(myDB, student, ADD);
         sqlite3_close(myDB);  // always close it when done
     }
 }
